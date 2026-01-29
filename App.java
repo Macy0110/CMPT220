c57bb5b176300220dfacf858b79d7cf0018345f4
Macy0110 <macyrobbins@hotmail.com>
2026-01-29 16:04:00 -0500

    Add files via upload

---
+++ App.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("How old are you? ");
+        Integer age= sc.nextInt();
+        System.out.println("You are" + age + "!");
+        System.out.println("What is 10 plus 37? ");
+        Integer num1= 10;
+        Integer num2= 37;
+        System.out.println(num1 + num2);
+        // I learned how to get input from a user (it's a bit harder than python and the syntax is different). 
+
+
+
+        
+
+
+         
+        
+    }
+}
